84bbd3394c8b430203b136dd63c2cd8971275bdd
devsetgo <mikeryan56@gmail.com>
2023-11-05 20:56:12 +0000

    The tests are functionally working now.

--- tests/test_database_connector.py
+++ tests/test_database_connector.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 # tests/test_database_connector.py
 
 import pytest
 from unittest.mock import MagicMock, patch, AsyncMock
 from devsetgo_toolkit.database_connector import AsyncDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 @pytest.mark.asyncio
-@patch('devsetgo_toolkit.database_connector.AsyncDatabase')
+@patch("devsetgo_toolkit.database_connector.AsyncDatabase")
 async def test_create_tables(mock_db_class):
     # Create a mock database instance with mocked engine and Base
     db_instance = mock_db_class.return_value
@@ -17,20 +20,22 @@ async def test_create_tables(mock_db_class):
     # Check if the function calls are correct
     db_instance.create_tables.assert_called_once()
 
+
 @pytest.mark.asyncio
-@patch('devsetgo_toolkit.database_connector.AsyncDatabase')
-async def test_get_db_session(mock_db_class):
-    # Create a mock database instance with mocked sessionmaker
-    db_instance = mock_db_class.return_value
-    mock_session = MagicMock()
-    mock_context_manager = AsyncMock()
-    mock_context_manager.__aenter__.return_value = mock_session
-    db_instance.sessionmaker.return_value = mock_context_manager
+@patch.object(AsyncSession, "__init__", return_value=None)
+@patch.object(AsyncSession, "commit")
+@patch.object(AsyncSession, "close")
+async def test_get_db_session(mock_close, mock_commit, mock_init):
+    # Create an instance of the real AsyncDatabase class
+    db_instance = AsyncDatabase(
+        {"database_uri": "sqlite+aiosqlite:///:memory:?cache=shared"}
+    )
 
-    # Call the function under test
+    # Use the context manager
     async with db_instance.get_db_session() as session:
-        # Check if the returned session is correct
-        assert session == mock_session
+        # Check if the correct session is returned
+        assert isinstance(session, AsyncSession)
 
-    # Check if the function calls are correct
-    db_instance.sessionmaker.assert_called_once()
+    # Check if commit and close were called once
+    mock_commit.assert_called_once()
+    mock_close.assert_called_once()
