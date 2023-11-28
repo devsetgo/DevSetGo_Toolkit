74e20418a031079d0820c64b7d6525273035cab7
devsetgo <mikeryan56@gmail.com>
2023-11-28 18:56:20 +0000

    updating for better configuration

--- devsetgo_toolkit/logger.py
+++ devsetgo_toolkit/logger.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
-import sys
 
 # Get a logger named after the module where this is called
 logger = logging.getLogger(__name__)
 
 
-def setup_logger(level=logging.INFO):
+def setup_logger(level=logging.WARNING, propagate=False):
     """
     Sets up the logger with the given log level.
 
@@ -19,13 +18,13 @@ def setup_logger(level=logging.INFO):
     # Check if the logger already has handlers, and if not, add a StreamHandler
     if not logger.handlers:
         # Create a StreamHandler that outputs to the console
-        ch = logging.StreamHandler(sys.stdout)  # Explicitly set to stdout
+        ch = logging.StreamHandler()  # Explicitly set to stdout
         # Set the log level of the StreamHandler
         ch.setLevel(level)
 
         # Create a Formatter that formats the log messages
         formatter = logging.Formatter(
-            "toolkit | %(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            " %(levelname)s -  %(name)s - %(asctime)s  - %(message)s"
         )
         # Set the Formatter of the StreamHandler to the created Formatter
         ch.setFormatter(formatter)
@@ -34,7 +33,7 @@ def setup_logger(level=logging.INFO):
         logger.addHandler(ch)
 
     # Prevent messages from being passed to the root logger's handlers
-    logger.propagate = False
+    logger.propagate = propagate
 
 
 # Set up the logger with the default log level
